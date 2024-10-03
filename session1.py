aeb448d532fbb810b3117e74f0fc2a7f70044614
mgonz376 <mgonz376@ucsc.edu>
2024-10-02 22:27:59 -0700

    session 1 for code journal 1.

---
+++ session1.py
@@ -0,0 +1,24 @@
+'''This program will say my name,favorite food, and movie'''
+
+#This function will introduce myself
+
+a = 'Hello! My name is Maya.'
+type(a)
+
+print('Hello! My name is Maya.')
+
+#This function will say my favorite food
+
+b = 'My favorite food is sushi.'
+
+type(b)
+
+print('My favorite food is sushi.')
+
+#This function will say my favorite movie
+
+c = 'Crazy Rich Asians is my favorite movie!'
+
+type(c)
+print('Crazy Rich Asians is my favorite movie!')
+
